14f8c2f557d290dba0726323a9b66ad680d7ae4d
Jayr Motta <jayrmotta@gmail.com>
2017-10-15 21:23:26 -0200

    Mudando a forma de acessar a url

--- js/anguvideo.js
+++ js/anguvideo.js
@@ -5,25 +5,31 @@
 'use strict';
 
 angular.module('anguvideo', ['anguvideo.vimeo', 'anguvideo.youtube', 'anguvideo.azure'])
-    .directive("anguvideo", ['$sce', '$interval', function ($sce, $interval) {
+    .directive("anguvideo", ['$interval', '$compile', function ($interval, $compile) {
         return {
             restrict: 'EA',
             scope: {
                 source: '=ngModel',
                 width: '@',
                 height: '@',
-								autoPlay: '@'
+								autoPlay: '@',
+                url: '@'
             },
             replace: true,
-            template: function(element, attrs) {
-              if (attrs.url.indexOf('vimeo') >= 0) {
-                return '<div><vimeo-player url="' + attrs.url + '"></vimeo-player></div>'
-              } else if (attrs.url.indexOf('youtu.be') >= 0 || attrs.url.indexOf("youtube.com") >= 0) {
-                return '<div><youtube-player url="' + attrs.url + '"></youtube-player></div>'
-              } else if (attrs.url.indexOf('windows') >= 0) {
-                return '<div><azure-media-player url="' + attrs.url + '"></azure-media-player></div>'
+            link: function(scope, element, attrs) {
+              var render = function(html) {
+                var childElement = $compile(html)(scope);
+                element.append(childElement);
+              }
+
+              if (scope.url.indexOf('vimeo') >= 0) {
+                render('<div><vimeo-player url="{{url}}"></vimeo-player></div>')
+              } else if (scope.url.indexOf('youtu.be') >= 0 || scope.url.indexOf("youtube.com") >= 0) {
+                render('<div><youtube-player url="{{url}}"></youtube-player></div>')
+              } else if (scope.url.indexOf('windows') >= 0) {
+                render('<div><azure-media-player url="{{url}}"></azure-media-player></div>')
               } else {
-                throw 'could not determine the right template for ' + attrs.url
+                throw 'could not determine the right template for ' + scope.url
               }
             }
         };
