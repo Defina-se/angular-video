ae6e2b8e638815b06d520d29d5e59b976c0ed0bb
Jayr Motta <jayrmotta@gmail.com>
2017-10-15 21:38:40 -0200

    Fazendo templates serem inline

--- components/azure/config.js
+++ components/azure/config.js
@@ -1,6 +1,9 @@
 angular.module('anguvideo.azure', ['anguvideo.azure.controller'])
   .component('azureMediaPlayer', {
-    templateUrl : './components/azure/component.html',
+    template : '<link href="//amp.azure.net/libs/amp/latest/skins/amp-default/azuremediaplayer.min.css" rel="stylesheet">' +
+                '<div class="anguvideo embed-responsive embed-responsive-16by9">' +
+                  '<video id="playerAzure" class="azuremediaplayer amp-default-skin" controls></video>' +
+                '</div>',
     controller : 'AzureMediaPlayerController',
     bindings : {
       url : '@'
--- components/vimeo/config.js
+++ components/vimeo/config.js
@@ -1,6 +1,8 @@
 angular.module('anguvideo.vimeo', ['anguvideo.vimeo.controller'])
   .component('vimeoPlayer', {
-    templateUrl : './components/vimeo/component.html',
+    template : '<div class="anguvideo embed-responsive embed-responsive-16by9">' +
+                    '<iframe id="playerVimeo" class="videoClass" type="text/html" ng-src="{{$ctrl.embedUrl | trusted}}" allowfullscreen frameborder="0"></iframe>' +
+                  '</div>',
     controller : 'VimeoPlayerController',
     bindings : {
       url : '@'
