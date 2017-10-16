4d2c0814c91ffb082f2cfd9b12bf44e98fc73603
Jayr Motta <jayrmotta@gmail.com>
2017-10-15 22:25:52 -0200

    Correção youtube player

--- components/youtube/config.js
+++ components/youtube/config.js
@@ -1,6 +1,5 @@
 angular.module('anguvideo.youtube', ['anguvideo.youtube.controller'])
   .component('youtubePlayer', {
-    templateUrl : './components/youtube/component.html',
     template : '<div class="anguvideo embed-responsive embed-responsive-16by9">' +
                     '<iframe id="playerYV" class="videoClass" type="text/html" ng-src="{{$ctrl.embedUrl | trusted}}" allowfullscreen frameborder="0"></iframe>' +
                   '</div>',
